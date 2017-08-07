f4508fe28873fc9c6964b488091b49d0b5e7eaf1
Alex José Silva <alexjosesilvati@gmail.com>
2017-08-07 16:34:05 -0300

    bug fix
    bug fix

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 //acesso ao banco de dados   
-$enderecobd = "http://127.0.0.1:49577";   
+$enderecobd = "https://127.0.0.1:49577";   
 $dbname = "localdb";       
                              
 //dados do banco             
@@ -9,9 +9,9 @@ $usuariobd = "azure";
 $senhabd = "6#vWHD_$";         
 
 //conectar ao banco                                                                                   
-$con   = mysql_connect($enderecobd,$usuariobd,$senhabd) or die ("Erro na conexão do Banco de Dados!");
+$con   = mysql_connect($enderecobd,$usuariobd,$senhabd) or die ("1-Erro na conexão do Banco de Dados!");
 
 //acessou ao banco dados	                                                 
-$selbd = mysql_select_db($dbname,$con) or die ("Erro em selecionar Tabela!");
+$selbd = mysql_select_db($dbname,$con) or die ("1-Erro em selecionar Tabela!");
 
 ?>
