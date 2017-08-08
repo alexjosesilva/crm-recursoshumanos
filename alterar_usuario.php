b7f7adb728dad9c64d531237638c986ccf6b63c5
Alex José Silva <turmawb@gmail.com>
2017-08-08 15:44:59 -0300

    bug
    bug

--- alterar_usuario.php
+++ alterar_usuario.php
@@ -10,12 +10,12 @@
 	$codigoUsuario	 	=	$_REQUEST['codigoUsuario'];
 	$senhaUsuario	 		=	$_REQUEST['senhaUsuario'];
 
-  echo "nomeUsuario: ".$nomeUsuario."<br>";
+  echo "nomeUsuario:   ".$nomeUsuario."<br>";
 	echo "codigoUsuario: ".$codigoUsuario."<br>";
-	echo "senhaUsuario: ".$senhaUsuario."<br>";
+	echo "senhaUsuario:  ".$senhaUsuario."<br>";
 
 	//query para atualizar o dado
-	$query = "UPDATE localdb.tusuario SET nomeUsuario=".$nomeUsuario."', codigoUsuario='".$codigoUsuario."',senhaUsuario='".$senhaUsuario."' where codigoUsuario=".$codigoUsuario;
+	$query = "UPDATE localdb.tusuario SET nomeUsuario='".$nomeUsuario."', codigoUsuario='".$codigoUsuario."', senhaUsuario='".$senhaUsuario."' where codigoUsuario='".$codigoUsuario."'";
 	$resultado = mysql_query($query,$con) or die ("2-erro em ATUALIZAR o banco!");
 
 	header('Location:adm.php');
