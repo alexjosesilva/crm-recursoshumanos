b0caf658d41d2e72c6e741bc11200320d07cd7a8
Alex José Silva <turmawb@gmail.com>
2017-08-08 14:51:42 -0300

    bug
    bug

--- teste1.php
+++ teste1.php
@@ -1,9 +1,17 @@
 <?php
+	include 'conexao.php';
 
 	$codigoUsuario = $_GET['codigo'];
 
 	if(!empty($codigoUsuario)){
 		echo $codigoUsuario;
+
+		$query   = "select * from tusuario where codigoUsuario=".$codigoUsuario;
+  	$dados   = mysql_query($query);
+  	//$usuario = mysql_affected_rows($dados));
+    var_dump($dados);
+
+
 	}
 
 ?>
