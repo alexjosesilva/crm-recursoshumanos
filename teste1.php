7e959e7db0b5dea3e958b398cee8c4cb07abd581
Alex José Silva <turmawb@gmail.com>
2017-08-08 14:55:27 -0300

    b
    b

--- teste1.php
+++ teste1.php
@@ -8,11 +8,11 @@
 
 		$query   = "select * from tusuario where codigoUsuario=".$codigoUsuario;
   	$dados   = mysql_query($query);
-  	$usuario = mysql_affected_rows($dados));
+  	//$usuario = mysql_affected_rows($dados));
     var_dump($usuario);
 
 		echo "<br>";
-		
+
 		echo isset($usuario)?$usuario['nomeUsuario']:"";
 
 	}
