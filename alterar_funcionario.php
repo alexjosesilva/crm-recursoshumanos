d43e3e412099d71d39858a2a9b25b5735a9de072
Alex José Silva <turmawb@gmail.com>
2017-08-08 17:41:17 -0300

    bug
    bug

--- alterar_funcionario.php
+++ alterar_funcionario.php
@@ -2,23 +2,24 @@
 	 //acesso ao banco
 	 include 'conexao.php';
 
-
 	//testa se foi enviado algum dado para esta pagina
-    if(isset($_REQUEST['codigoFuncao'])){
+    if(isset($_REQUEST['codigoFuncionario'])){
 
-	$descricaoFuncao 	 =	$_REQUEST['descricaoFuncao'];
-	$codigoFuncao	 	 =	$_REQUEST['codigoFuncao'];
-
+			$nomeFuncionario 	 		 =	$_REQUEST['nomeFuncionario'];
+			$codigoFuncionario	 	 =	$_REQUEST['codigoFuncionario'];
+			$salario							 =	$_REQUEST['salario'];
+			$dataNascimento				 =	$_REQUEST['dataNascimento'];
+			$funcao								 =	$_REQUEST['funcao'];
 
 	//query para atualizar o dado
-	$query = "UPDATE tfuncoes SET descricaoFuncao='".$descricaoFuncao."', codigoFuncao='".$codigoFuncao."' where codigoFuncao='".$codigoFuncao."'";
+	$query = "UPDATE tfuncionario SET nomeFuncionario='".$nomeFuncionario."', codigoFuncionario='".$codigoFuncionario."',salario='".$salario."',dataNascimento='".$dataNascimento."',funcao='".$funcao."' where codigoFuncionario='".$codigoFuncionario."'";
 
 	$resultado = mysql_query($query,$con) or die ("erro em ATUALIZAR o banco!");
-	header('Location:funcao.php');
+	header('Location:funcionario.php');
 
     }
 	else{//caso não tenha nenhum dado!
-		header("Location:funcao.php");
+		header("Location:funcionario.php");
 	}
 
 ?>
