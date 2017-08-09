b3a8daa5b41e67462fd83032dbf80512e479d35a
Alex José Silva <turmawb@gmail.com>
2017-08-08 21:27:21 -0300

    bug
    bug

--- funcionario.php
+++ funcionario.php
@@ -62,12 +62,12 @@ if(!empty($_GET['codigoAltFuncionario'])){
 
 	  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	  <link rel="stylesheet" href="/resources/demos/style.css">
-	  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	 
+	 
 	  <script>
-	  $( function() {
-	    $( "#dataNascimento" ).datepicker();
-	  } );
+		  $( function() {
+		    $( "#dataNascimento" ).datepicker();
+		  } );
 	  </script>
 
 	</head>
